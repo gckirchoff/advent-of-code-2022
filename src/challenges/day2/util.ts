2ee78df9f3b4338a20338a48ab2da9d11c08201a
Gregory Kirchoff <gckirchoff@gmail.com>
2022-12-03 16:38:12 -0500

    Made abstract class

--- src/challenges/day2/util.ts
+++ src/challenges/day2/util.ts
@@ -1,55 +1,18 @@
 import { Challenge } from '../../types';
-import { data } from './data';
-import { Choice, Result } from './types';
 import type { Round } from './types';
 
-abstract class Day1 implements Challenge<number> {
-  part = 'day 2 part 1';
+export abstract class Day1 implements Challenge<number> {
+  part: string;
   private data: string;
-  private scoreIGet = {
-    X: {
-      value: Choice.ROCK,
-      theirChoice: {
-        A: Result.DRAW,
-        B: Result.LOSE,
-        C: Result.WIN,
-      },
-    },
-    Y: {
-      value: Choice.PAPER,
-      theirChoice: {
-        A: Result.WIN,
-        B: Result.DRAW,
-        C: Result.LOSE,
-      },
-    },
-    Z: {
-      value: Choice.SCISSORS,
-      theirChoice: {
-        A: Result.LOSE,
-        B: Result.WIN,
-        C: Result.DRAW,
-      },
-    },
-  };
 
-  constructor(data: string) {
+  constructor(part: string, data: string) {
+    this.part = part;
     this.data = data;
   }
 
-  getAnswer = () => {
-    const rounds = this.getCleanedData();
-    const myScore = rounds.reduce((acc, [them, me]) => {
-      const myChoice = this.scoreIGet[me];
-      acc += myChoice.value + myChoice.theirChoice[them];
-      return acc;
-    }, 0);
-    return myScore;
-  };
+  abstract getAnswer: () => number;
 
-  private getCleanedData = (): Round[] => {
+  protected getCleanedData = (): Round[] => {
     return this.data.split('\n').map((round) => round.split(' ') as Round);
   };
 }
-
-export const dayTwoPartOne = new D2P1(data);
